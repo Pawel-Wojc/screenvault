e0b648cccf68f62e9be42047be696fcc5153207c
pawel-pawel <pawelitwojcik@gmail.com>
2024-09-02 19:25:16 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './authorization/login/login.component';
+import { SignupComponent } from './authorization/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'screenvault' },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+];
