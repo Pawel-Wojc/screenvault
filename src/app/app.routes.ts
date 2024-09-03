290c2bdf0fbe7f57a5420ff98acbe4bc3215856c
pawel-pawel <pawelitwojcik@gmail.com>
2024-09-03 19:28:49 +0200

    route upate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './authorization/login/login.component';
 import { SignupComponent } from './authorization/signup/signup.component';
+import { NewPostComponent } from './post/new-post/new-post.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, title: 'screenvault' },
+  { path: '', component: HomeComponent, title: 'ScreenVault' },
 
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  {
+    path: 'new-post',
+    component: NewPostComponent,
+  },
 ];
