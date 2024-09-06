eda8af9f948d28e8fa9aa14ec7180019c99dbfe0
lukasz <lukasz55szary@gmail.com>
2024-09-06 23:00:29 +0200

    create singup form

--- src/app/authorization/signup/signup.component.ts
+++ src/app/authorization/signup/signup.component.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { newUser } from '../../interfaces/newUser';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  newUser!: newUser;
+  singUpForm!: FormGroup;
+  passwordInvalid = false;
+
+  constructor(private formBuilder: FormBuilder){
+    this.singUpForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      email: ['', [Validators.required,Validators.email /*pattern(/^[\w]+@([\w-]+\.)+[\w]{2,4}$/)*/]],
+      password1: ['', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/)]],
+      password2: ['', [Validators.required, /*this.validatePassword*/]] 
+    })
+
+  }
+  
+validatePassword(control: AbstractControl){
+    const password1 = this.singUpForm.get('password1')?.value as string;
+    const password2 = control.value as string;
+    
+    if('password1' !== password2){
+      return {passwordMissMatch: true}
+    }
+
+    return null;
+  };
+
+  submitSingUpForm(){
+    if(this.singUpForm.value.password1 === this.singUpForm.value.password2)
+    {
+      this.passwordInvalid = false;
+      console.log( this.singUpForm.value);
+    }
+    else{
+      this.passwordInvalid = true;
+    } 
+    
+  }
+}
---
+++ src/app/interfaces/newUser.ts
@@ -0,0 +1,6 @@
+export interface newUser {
+    userName: Text,
+    email: Text,
+    password1: Text,
+    password2: Text,
+}
