457873d24d493392b3d6eb86eeba0da4c599dadc
lukasz <lukasz55szary@gmail.com>
2024-09-10 23:15:24 +0200

    nge new-post and login/signup css

--- src/app/post/new-post/new-post.component.ts
+++ src/app/post/new-post/new-post.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-post',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './new-post.component.html',
   styleUrl: './new-post.component.css'
 })
 export class NewPostComponent {
+  //for css use
+  dragging = false;
 
+  // Enter drop zone event
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    this.dragging = true;
+  }
+
+  // Leave drop zone event
+  onDragLeave(event: DragEvent) {
+    this.dragging = false;
+  }
+
+  // Drop file event
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    this.dragging = false;
+
+    if (event.dataTransfer?.files) {
+      this.handleFiles(event.dataTransfer.files);
+    }
+  }
+
+  // action when file selected manualy
+  onFileSelected(event: Event){
+    const target = event.target as HTMLInputElement;
+    if(target.files){
+      this.handleFiles(target.files);
+    }
+  }
+
+  handleFiles(files: FileList){
+    console.log(files);
+  }
 }
