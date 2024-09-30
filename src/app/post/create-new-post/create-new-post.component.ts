d8b38126cf0a070ae96f8f2dac6b2f25f26ccf69
lukasz <lukasz55szary@gmail.com>
2024-09-30 20:44:59 +0200

    file robot config adjustments

--- src/app/post/create-new-post/create-new-post.component.ts
+++ src/app/post/create-new-post/create-new-post.component.ts
@@ -31,76 +31,28 @@ export class CreateNewPostComponent {
     const filerobotImageEditor = new FilerobotImageEditor(container as HTMLElement, {
       source: this.fileUrl, // You can provide an image URL here
       //source: "https://scaleflex.airstore.io/demo/stephen-walker-unsplash.jpg",
-      //tools: ['adjust', 'effects', 'filters', 'resize', 'crop', 'rotate'],
-        
+      onSave: (editedImageObject, designState) =>{
+        alert('owo shy')
+      },
       annotationsCommon: {
         fill: '#ff0000',
       },
+      defaultSavedImageName: this.file?.name,
+      defaultSavedImageQuality: 1,
+      defaultSavedImageType: this.file?.type,
+      closeAfterSave: true,
       Text: { text: 'Text' },
       Rotate: { angle: 90, componentType: 'slider' },
-      /*translations: {
-        profile: 'Profile',
-        coverPhoto: 'Cover photo',
-        facebook: 'Facebook',
-        socialMedia: 'Social Media',
-       // fbProfileSize: '180x180px',
-       // fbCoverPhotoSize: '820x312px',
-        
-      },
-      Crop: {
-        presetsItems: [
-          {
-            titleKey: 'classicTv',
-            descriptionKey: '4:3',
-            ratio: 4 / 3,
-            // icon: CropClassicTv, // optional, CropClassicTv is a React Function component. Possible (React Function component, string or HTML Element)
-          },
-          {
-            titleKey: 'cinemascope',
-            descriptionKey: '21:9',
-            ratio: 21 / 9,
-            // icon: CropCinemaScope, // optional, CropCinemaScope is a React Function component.  Possible (React Function component, string or HTML Element)
-          },
-        ],
-        presetsFolders: [
-          {
-            titleKey: 'socialMedia', // will be translated into Social Media as backend contains this translation key
-            // icon: Social, // optional, Social is a React Function component. Possible (React Function component, string or HTML Element)
-            groups: [
-              {
-                titleKey: 'facebook',
-                items: [
-                  {
-                    titleKey: 'profile',
-                    width: 180,
-                    height: 180,
-                    descriptionKey: 'fbProfileSize',
-                  },
-                  {
-                    titleKey: 'coverPhoto',
-                    width: 820,
-                    height: 312,
-                    descriptionKey: 'fbCoverPhotoSize',
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },*/
-      tabsIds: [TABS.ADJUST, TABS.ANNOTATE, TABS.FINETUNE, TABS.FILTERS ] as string[], // or ['Adjust', 'Annotate', 'Watermark']
-      defaultTabId: TABS.ADJUST, // or 'Annotate'
-      //defaultToolId: TOOLS.TEXT,
+      tabsIds: [TABS.ADJUST, TABS.ANNOTATE, TABS.FINETUNE, TABS.FILTERS ] as string[], 
+      defaultTabId: TABS.ADJUST, 
   
     } as FilerobotImageEditorConfig);
 
-    filerobotImageEditor.render();
+    filerobotImageEditor.render({
+      onClose: () => {
+        filerobotImageEditor.terminate();
+      } ,
+    } as any );
   }
 
-  
-
 }
-
-  
-   
-
