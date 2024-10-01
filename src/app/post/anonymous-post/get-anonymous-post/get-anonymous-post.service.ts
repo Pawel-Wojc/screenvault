ea290831b59ce3affa06e4379bd3498044341f78
pawel-pawel <pawelitwojcik@gmail.com>
2024-10-01 11:23:16 +0200

    get-anonymous post component

--- src/app/post/anonymous-post/get-anonymous-post/get-anonymous-post.service.ts
+++ src/app/post/anonymous-post/get-anonymous-post/get-anonymous-post.service.ts
@@ -5,4 +5,10 @@ import * as myGlobals from '../../../global';
 @Injectable({ providedIn: 'root' })
 export class GetAnonymousPostService {
   private httpClient = inject(HttpClient);
+
+  getAnonymousPost(linkId: string) {
+    const url = myGlobals.apiLink + '/post/anonymous/get';
+    const params = new HttpParams().set('linkId', linkId);
+    return this.httpClient.get(url, { params, responseType: 'blob' });
+  }
 }
