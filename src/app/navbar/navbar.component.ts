761865a176bbe74cd7dcab8c72742253a09aee27
Paweł Wójcik <62841636+Pawel-Wojc@users.noreply.github.com>
2024-10-17 11:03:33 +0200

    navbar logged/not logged

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../authorization/auth.service';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isLoggedIn$!: Observable<boolean>;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.isLoggedIn$ = this.authService.isLoggedIn;
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
