155882b00db8d1b440ef91bccefa0512e24d16b2
lukasz <lukasz55szary@gmail.com>
2024-10-26 22:00:00 +0200

    work progres on comment section and some refactoring

--- src/app/home/comment-section/comment-section.component.ts
+++ src/app/home/comment-section/comment-section.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-comment-section',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './comment-section.component.html',
   styleUrl: './comment-section.component.css'
 })
 export class CommentSectionComponent {
+  imgSrc:string ='dsad';
 
 }
