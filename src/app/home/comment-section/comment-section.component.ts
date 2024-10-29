9f7024f8ef727938cab9ba311de0a71500668adc
lukasz <lukasz55szary@gmail.com>
2024-10-29 22:16:16 +0100

    css for comment section

--- src/app/home/comment-section/comment-section.component.ts
+++ src/app/home/comment-section/comment-section.component.ts
@@ -1,22 +1,25 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { FormBuilder, Validators, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { CommentServiceService } from './comment-service.service'
+import { CommentServiceService } from './comment-service.service';
+import { Comment } from './comment';
 
 @Component({
   selector: 'app-comment-section',
   standalone: true,
   imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './comment-section.component.html',
-  styleUrl: './comment-section.component.css'
+  styleUrl: './comment-section.component.css',
+  providers: [DatePipe]
 })
 export class CommentSectionComponent {
   imgSrc: string ='dsad';
-
+  comments: Comment[] = [];
 
   private formBuilder = inject(FormBuilder);
   private commentService = inject(CommentServiceService);
+  private datePipe = inject(DatePipe);
 
   addCommentForm: FormGroup;
 
@@ -27,14 +30,25 @@ export class CommentSectionComponent {
   }
 
   ngOnInit(){
-    alert('implement view counter api call');
-    alert('implement get call for picture');
-    alert(' implement get comments');
+    alert('implement view counter api call implement get call for picture  implement get comments post comment fill comment obj');
+
+
+    this.comments = this.commentService.getComments();
+   // console.log(this.commentService.getComments());
+   // console.log(this.comments);
+
   }
 
   saveComment(){
     alert("implement me!!!!!!");
-    alert(this.addCommentForm.value.comment);
+    const date = new Date();
+    let newComment: Comment = {
+      id: '',
+      userName: '',
+      text: this.addCommentForm.value.comment,
+      postedOn: this.datePipe.transform(date, 'yyyy-MM-dd HH:mm:ss') as string,
+    };
+    console.log(newComment);
     this.addCommentForm.reset();
   }
 
--- src/app/home/comment-section/comment-service.service.ts
+++ src/app/home/comment-section/comment-service.service.ts
@@ -1,9 +1,151 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Comment } from './comment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentServiceService {
+  private apiURL: string = "";
 
+  private httpClient = inject(HttpClient);
   constructor() { }
+
+  getComments(){
+    return [
+    {
+    id: 'FSDFDGFRBVBD',
+    userName: 'Tomek',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+    postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+    {
+      id: 'FSDFDGFRBVBD',
+      userName: 'Tomek',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco',
+      postedOn: new Date('2001-12-31'),
+    },
+                                                                                                                                                                                                                                                                              
+    ];
+  }
 }
