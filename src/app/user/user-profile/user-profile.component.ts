a013a0e969fc5a50dc41e82aec13c485d8be9f95
pawel-pawel <pawelitwojcik@gmail.com>
2024-11-03 22:50:54 +0100

    folder and avatar

--- src/app/user/user-profile/user-profile.component.ts
+++ src/app/user/user-profile/user-profile.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.css'
+  styleUrl: './user-profile.component.css',
 })
 export class UserProfileComponent {
-
+  numbers = Array.from({ length: 11 }, (_, i) => i + 1);
 }
