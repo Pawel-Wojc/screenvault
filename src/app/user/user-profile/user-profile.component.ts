4a3f18c08c20bf0620b9b1c9bcdfd255f4007b45
pawel-pawel <pawelitwojcik@gmail.com>
2024-11-05 22:09:17 +0100

    making dialog

--- src/app/user/user-profile/user-profile.component.ts
+++ src/app/user/user-profile/user-profile.component.ts
@@ -1,5 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Component, inject, Inject } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-user-profile',
@@ -9,5 +19,35 @@ import { Component } from '@angular/core';
   styleUrl: './user-profile.component.css',
 })
 export class UserProfileComponent {
+  Email: any;
+  Username: any;
+  Collections: any;
+  private snackBar = inject(MatSnackBar);
+  private dialog = inject(MatDialog);
+  deleteProfilePicture() {}
+  openDialog() {}
   numbers = Array.from({ length: 11 }, (_, i) => i + 1);
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'Close', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
+}
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: 'dialog.html',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogTitle,
+    MatDialogContent,
+  ],
+})
+export class Dialog {
+  readonly dialog = inject;
 }
