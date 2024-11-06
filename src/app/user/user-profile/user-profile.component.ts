10f266d7536337b6b42042ba5338d93a75f72fd8
Paweł Wójcik <62841636+Pawel-Wojc@users.noreply.github.com>
2024-11-06 16:16:36 +0100

    creating user profile

--- src/app/user/user-profile/user-profile.component.ts
+++ src/app/user/user-profile/user-profile.component.ts
@@ -1,53 +1,81 @@
 import { CommonModule } from '@angular/common';
-import { Component, Component, inject, Inject } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import {
-  MatDialog,
-  MatDialogActions,
-  MatDialogClose,
-  MatDialogContent,
-  MatDialogRef,
-  MatDialogTitle,
-} from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
+  Component,
+  ElementRef,
+  inject,
+  signal,
+  ViewChild,
+} from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { SimpleDialog } from '../../shared/simple-dialog/simple-dialog';
+import { CollectionsComponent } from '../collections/collections.component';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTooltipModule, CollectionsComponent],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css',
 })
 export class UserProfileComponent {
-  Email: any;
-  Username: any;
-  Collections: any;
+  profilePhotoPlaceholder: string = 'demo-avatar.png';
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
   private snackBar = inject(MatSnackBar);
   private dialog = inject(MatDialog);
-  deleteProfilePicture() {}
-  openDialog() {}
-  numbers = Array.from({ length: 11 }, (_, i) => i + 1);
-  openSnackBar(message: string) {
-    this.snackBar.open(message, 'Close', {
-      duration: 3000,
-      horizontalPosition: 'center',
-      verticalPosition: 'top',
-    });
-  }
-}
+  userPhoto = signal<string | ArrayBuffer | null>(this.profilePhotoPlaceholder);
+  userEmail = signal('email');
+  userName = signal('username');
 
-@Component({
-  selector: 'app-dialog',
-  templateUrl: 'dialog.html',
-  standalone: true,
-  imports: [
-    MatButtonModule,
-    MatDialogActions,
-    MatDialogClose,
-    MatDialogTitle,
-    MatDialogContent,
-  ],
-})
-export class Dialog {
-  readonly dialog = inject;
+  changeUserProfilePicture(event: Event) {
+    const target = event.target as HTMLInputElement;
+    console.log(target.files);
+    if (target.files) {
+      const file = target.files[0];
+      if (
+        file.type === 'image/png' ||
+        file.type === 'image/jpeg' ||
+        file.type === 'image/jpg'
+      ) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.userPhoto.set(reader.result);
+        };
+        reader.readAsDataURL(target.files[0]);
+        this.fileInput.nativeElement.value = ''; //setting input to empty, then if we delete selected and select again same file, input will trigger change
+        //call api
+        console.log('call api to save user profile picture changes');
+      } else {
+        this.snackBar.open('Available types: png, jpg, jpeg', 'Close', {
+          duration: 2000,
+        });
+      }
+    }
+  }
+
+  deleteProfilePicture(event: Event) {
+    if (this.userPhoto() !== this.profilePhotoPlaceholder) {
+      const dialogRef = this.dialog.open(SimpleDialog, {
+        width: '300px',
+        data: {
+          title: 'Delete Profile Picture',
+          message: 'Are you sure you want to delete your profile picture?',
+          confirmButtonText: 'Yes',
+          cancelButtonText: 'No',
+        },
+      });
+      dialogRef.afterClosed().subscribe((result) => {
+        if (result == true) {
+          this.userPhoto.set(this.profilePhotoPlaceholder);
+          console.log('Call api to delete photo');
+        }
+      });
+    }
+  }
+
+  changeUserPassword() {
+    //implementation??
+  }
 }
