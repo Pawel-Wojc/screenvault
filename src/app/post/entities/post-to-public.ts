0aa854694b80d4f75036ae8b0ddfae49a4121668
lukasz <lukasz55szary@gmail.com>
2024-12-03 19:58:13 +0100

    refactor post public

--- src/app/post/entities/post-to-public.ts
+++ src/app/post/entities/post-to-public.ts
@@ -1,11 +1,9 @@
 export class PostToPublic {
     title: string;
-    postedOn: Date;
     isPublic: boolean;
 
     constructor(title: string, isPublic: boolean){
         this.title = title;
-        this.postedOn = new Date();
         this.isPublic = isPublic;
     }
 }
