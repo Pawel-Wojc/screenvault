f93d41518b5f81693118d0957858f4b40b9999ac
lukasz <lukasz55szary@gmail.com>
2024-12-12 21:45:32 +0100

    link to api

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,2 +1,2 @@
-export const apiLink =  '/api'; // 'https://localhost:44308';
+export const apiLink =  'https://screenvaultapi-cxdaatead8bpczde.westeurope-01.azurewebsites.net'; // 'https://localhost:44308';
 export const frontLink = 'https://gray-river-074bfb703.4.azurestaticapps.net';
