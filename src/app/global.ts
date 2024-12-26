204feb179699882c9a8724ede49015a34a61ff7b
lukasz <lukasz55szary@gmail.com>
2024-12-26 16:09:15 +0100

    add proper api and front url's

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,2 +1,2 @@
-export const apiLink =  'http://localhost:8080'; // 'https://localhost:44308';
-export const frontLink = 'http://localhost:4200';
+export const apiLink =  'https://screenvaultapi-cxdaatead8bpczde.polandcentral-01.azurewebsites.net'; //'http://localhost:8080'; 
+export const frontLink = 'https://gray-river-074bfb703.4.azurestaticapps.net/'; //'http://localhost:4200'; 
