d3a5bf2c08a122688dba20c172b2862a1750e7ab
lukasz <lukasz55szary@gmail.com>
2024-12-27 19:44:04 +0100

    add routing config for azure

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,2 +1,3 @@
-export const apiLink =  'https://screenvaultapi-cxdaatead8bpczde.polandcentral-01.azurewebsites.net'; //'http://localhost:8080'; 
+//export const apiLink =  'http://localhost:8080'; //'https://screenvaultapi-cxdaatead8bpczde.polandcentral-01.azurewebsites.net'; 
+export const apiLink = 'https://screenvaultapi-cxdaatead8bpczde.polandcentral-01.azurewebsites.net'; 
 export const frontLink = 'https://gray-river-074bfb703.4.azurestaticapps.net'; //'http://localhost:4200'; 
